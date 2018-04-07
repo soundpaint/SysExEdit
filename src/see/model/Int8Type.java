b2ccb732150c4c659aa16d52013f50a6f4ad92f9
Juergen Reuter <lily-devel@soundpaint.org>
2018-04-07 15:58:29 +0200

    Fix bulk dump: Minor clean-up
    * Make Int8Type string display options more flexible,
      such that DB50XG does not need to explicitly enumerate
      all hexadecimal values of valid MIDI device IDs.

--- src/see/model/Int8Type.java
+++ src/see/model/Int8Type.java
@@ -28,6 +28,10 @@ public class Int8Type implements ValueType
 {
   private final int lowerBound;
   private final int radix;
+  private final boolean fillWithLeadingZeroes;
+  private final String displayPrefix;
+  private final String displaySuffix;
+  private final byte minWidth;
 
   /**
    * A predefined Int8Type for arbitrary values n in the range 0x00
@@ -56,7 +60,7 @@ public class Int8Type implements ValueType
    */
   public Int8Type(final int lowerBound)
   {
-    this(lowerBound, 10);
+    this(lowerBound, 10, false, "", "", (byte)0);
   }
 
   /**
@@ -67,11 +71,29 @@ public class Int8Type implements ValueType
    * maps to.
    * @param radix The radix to use when creating a numeric display
    * value from the integer value.
+   * @param fillWithLeadingZeroes If minWidth is larger than the
+   * actual number of digits to display, then fill the remaining
+   * characters with leading zeroes.
+   * @param displayPrefix A string to add before the actual digits
+   * of the number.
+   * @param displaySuffix A string to add after the actual digits
+   * of the number.
+   * @param minWidth The minimum number of characters that the
+   * generated String will cover (including display prefix and display
+   * suffix).
    */
-  public Int8Type(final int lowerBound, final int radix)
+  public Int8Type(final int lowerBound, final int radix,
+                  final boolean fillWithLeadingZeroes,
+                  final String displayPrefix,
+                  final String displaySuffix,
+                  final byte minWidth)
   {
     this.lowerBound = lowerBound;
     this.radix = radix;
+    this.fillWithLeadingZeroes = fillWithLeadingZeroes;
+    this.displayPrefix = displayPrefix;
+    this.displaySuffix = displaySuffix;
+    this.minWidth = minWidth;
   }
 
   public int getLowerBound()
@@ -94,6 +116,32 @@ public class Int8Type implements ValueType
     return radix;
   }
 
+  public boolean getFillWithLeadingZeroes()
+  {
+    return fillWithLeadingZeroes;
+  }
+
+  public String getDisplaySuffix()
+  {
+    return displaySuffix;
+  }
+
+  public int getMinWidth()
+  {
+    return minWidth;
+  }
+
+  private static String createFill(final char ch, final int size)
+  {
+    if (size <= 0) {
+      // shortcut for better performance
+      return "";
+    }
+    final StringBuffer sb = new StringBuffer();
+    for (int i = 0; i < size; i++) sb.append(ch);
+    return sb.toString();
+  }
+
   /**
    * Returns a String that represents the specified value according to
    * the specification of this Int8Type.
@@ -101,7 +149,18 @@ public class Int8Type implements ValueType
   public String getDisplayValue(final int value)
   {
     final long index = value - lowerBound;
-    return Integer.toString((int)index, radix);
+    final long nonNegativeIndex = index >= 0 ? index : -index;
+    final String digits = Integer.toString((int)nonNegativeIndex, radix);
+    final String sign = index >= 0 ? "" : "-";
+    final int width =
+      displayPrefix.length() +
+      sign.length() +
+      digits.length() +
+      displaySuffix.length();
+    final int charsToAdd = minWidth - width;
+    final String fill =
+      createFill(fillWithLeadingZeroes ? '0' : ' ', charsToAdd);
+    return displayPrefix + fill + sign + digits + displaySuffix;
   }
 
   /**
